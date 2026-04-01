958b28070c104f2d5423ead53afd6807b5477bf6
Elvijs <s22leiselvi@venta.lv>
2026-04-01 11:17:38 +0300

    Added data get request

--- src/main/java/lv/venta/controller/SimpleController.java
+++ src/main/java/lv/venta/controller/SimpleController.java
@@ -1,8 +1,11 @@
 package lv.venta.controller;
 
+import java.util.Random;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
 
 @Controller
 @RequestMapping("/simple")
@@ -12,5 +15,14 @@ public class SimpleController {
 	public String getShowPage() {
 		System.out.println("Controller has been called");
 		return "show-page"; //show-page.html is shown, html files are put in templates
+	}
+	
+	@GetMapping("/data")
+	public String getDataInPage(Model model) {
+		System.out.println("Data controller has been called");
+		Random rand = new Random();
+		String data = "@Elvijs " + rand.nextInt(1, 1000);
+		model.addAttribute("package", data);
+		return "show-data-page";
 	}
 }
