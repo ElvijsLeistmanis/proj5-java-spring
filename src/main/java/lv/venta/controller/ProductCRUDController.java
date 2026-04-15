0e2e51f298072f0435134ab6935cd6d839272fbd
Elvijs <s22leiselvi@venta.lv>
2026-04-15 12:12:08 +0300

    idk im shutting down mentally

---
+++ src/main/java/lv/venta/controller/ProductCRUDController.java
@@ -0,0 +1,27 @@
+package lv.venta.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lv.venta.service.IProductCRUDService;
+
+@Controller
+@RequestMapping("/product/crud")
+public class ProductCRUDController {
+	@Autowired
+	private IProductCRUDService prodService;
+	
+	@GetMapping("/all")
+	public String getAllProduct(Model model) {
+		
+		try {
+			
+		}
+		catch (Exception e) {
+			
+		}
+	}
+}
